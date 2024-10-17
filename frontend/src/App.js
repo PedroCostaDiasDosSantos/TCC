740d070550b9afae86761619e7888250371e3a82
PedroCostaDiasDosSantos <pentrola400@gmail.com>
2024-10-17 00:34:48 -0300

    pagina-de-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,54 @@
 import './App.css';
-import Group1 from './components/images/Group1.png'
-import logo from './components/images/logo.png'
+import Group1 from './assets/images/Group2.png'
+import User from './assets/images/user.png'
+import { FaUser, FaLock } from 'react-icons/fa'
+
+import { useState } from 'react';
 
 function App() {
+
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleSubmit = (event) => {
+    event.prevenDefault();
+
+    alert("Enviando" + login + " - " + senha)
+  };
+
   return (
     <div className='App'>
-      <div className='roxo'>
-        <img src={Group1}/>
+      <div className='container'>
+        <div className='roxo'>
+          <img src={Group1} />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Bem Vindo(a)</h1>
+          <img className='User' src={User}/>
+          <div className='input-field'>
+            <h3>Insira seu email ou numero de celular:</h3>
+            <input type='email' placeholder='' 
+            onChange={(e) => setLogin(e.target.value)} />
+          </div>
+          <div className='input-field'>
+            <h3>Senha:</h3>
+            <input type='password' placeholder=''
+            onChange={(e) => setSenha(e.target.value)} />
+          </div>
+
+          <div className='recall-forget'>
+            <label>
+              <input type='checkbox' />
+              Lembre de mim
+            </label>
+            <a href='#'>Esqueceu a senha</a>
+          </div>
+          
+          <button>Entrar</button>
+
+        </form>
       </div>
-      <div className='logo'>
-        <img src={logo}/>
-      </div>
+
     </div>
 
   );
