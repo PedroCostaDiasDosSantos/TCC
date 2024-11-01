ca512d93ac523af837f6d2d380b1c2cc6429ff91
salustiano <ra56242444879@aconsfatima.org.br>
2024-11-01 17:41:32 -0300

    pag principal

--- frontend/src/pages/principal/index.jsx
+++ frontend/src/pages/principal/index.jsx
@@ -1,83 +1,26 @@
 import './index.scss';
-import Menu from '../../components/Menu';
-import Slider from 'react-slick';
+import Menu from '../../components/Menu/'
 
 
 export default function inicial(){
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
-    };
-
-    function NextArrow(props) {
-        const { onClick } = props;
-        return <div className="slick-arrow slick-next" onClick={onClick} />;
-    }
-
-    function PrevArrow(props) {
-        const { onClick } = props;
-        return <div className="slick-arrow slick-prev" onClick={onClick} />;
-    }
-
-    const CarrosselProdutos = () => (
-        <Slider {...settings}>
-            <div>
-                <img className="azul" src="/assets/images/niketnazul.png" alt="Tênis" />
-                <h1 className="n">Tênis</h1>
-            </div>
-            <div>
-                <img className="milan" src="/assets/images/milan.png" alt="Camisa Milan" />
-                <h2 className="time">Camisas de Time</h2>
-            </div>
-            <div>
-                <img className="high" src="/assets/images/camisetahigh.png" alt="Camiseta" />
-                <h3 className="cami">Camisetas</h3>
-            </div>
-            <div>
-                <img className="blusa" src="/assets/images/blusa.png" alt="Blusa" />
-                <h4 className="blu">Blusas</h4>
-            </div>
-        </Slider>
-    );
 
     const Div1 = () => <div className='inicial'>
            
-        <img className='logoo' src="/assets/images/logo.png" alt="" />
+        
 
         <h1 className='linqui'>
             <a className='link' id='sla1' href='#'>Lançamentos</a>
             <a className='link' id='sla2' href='#'>Masculino</a>
             <a className='link' id='sla3' href='#'>Feminino</a>
 
-            <input className='procurar' type="search" />
-            <img className='procurar2' src="/assets/images/procurar.png" alt="a " />
+            <input id='busca' type="text" class="search-input" placeholder="Pesquisar..." />
+            <button class="search-button">Buscar</button>
         </h1> 
          </div>
 
 
-    const Div2 = () => <div className='fot'>
+    const Div2 = () => <div className='fotos'>
             <img className='logo' src='/assets/images/muie.png' alt='a' /> 
     </div>
 
@@ -106,45 +49,73 @@ export default function inicial(){
             <img className='new' id="teste6" src='/assets/images/new.png' alt='a' />
     </div>
 
-    const Div8 = () => <div className='linhass'>
+
+     const Div8 = () => <div className='nome'>
+    <h1 className='teniss' id="teste12">Adidas</h1>
+    <h2 className='teniss' id="teste13">Puma</h2>
+    <h2 className='teniss' id="teste14">Nike</h2>
+    <h3 className='teniss' id="teste15">New Balance</h3>
     </div>
 
-    const Div9 = () => <div className='tenis'>
-         <img className='pisante' id="teste9" src='/assets/images/campus.png' alt='a' />
-         <img className='pisante' id="teste10" src='/assets/images/tn.png' alt='a' />
-         <img className='pisante' id="teste11" src='/assets/images/nb.png' alt='a' />
+    
+    const Div9 = () => <div className='linhass'>
     </div>
 
-    const Div10 = () => <div className='nome'>
-        <h1 className='teniss' id="teste12">Adidas</h1>
-        <h2 className='teniss' id="teste13">Puma</h2>
-        <h2 className='teniss' id="teste14">Nike</h2>
-        <h3 className='teniss' id="teste15">New Balance</h3>
+    const Div10 = () => <div className='tenis'>
+    <img className='pisante' id="teste9" src='/assets/images/campus.png' alt='a' />
+    <img className='pisante' id="teste10" src='/assets/images/tn.png' alt='a' />
+    <img className='pisante' id="teste11" src='/assets/images/nb.png' alt='a' />
     </div>
 
-    const Div11 = () => <div className='linhasss'>
+    const Div11 = () => <div className='nome'>
+        <h1 id='teste20'>Adidas</h1>
+        <h2 id='teste21'>Nike</h2>
+        <h3 id='teste22'>New Balance</h3>
     </div>
 
-    const Div12 = () => <div className='produtos'>
+    const Div12 = () => <div className='linhasss'>
+    </div>
+
+    const Div13 = () => <div className='produtos'>
         <h1>Produtos</h1>
     </div>
 
-    const Div13 = () => <div className='produtossss'>
-        <a href='#'>Ver Tudo</a>
+    const Div14 = () => <div className='produtossss'>
+        
+        <button id="produtoss" className="botao-transparente">
+            Ver Tudo
+        </button>
+        
     </div>
 
-    const Div14 = () => <div className='modelo'>
+    const Div15 = () => <div className='modelo'>
          <img className='azul' id="teste19" src='/assets/images/niketnazul.png' alt='a' />
          <img className='milan' id="teste16" src='/assets/images/milan.png' alt='a' />
          <img className='high' id="teste17" src='/assets/images/camisetahigh.png' alt='a' />
          <img className='blusa' id="teste18" src='/assets/images/blusa.png' alt='a' />
     </div>
 
-    const Div15 = () => <div className='name'>
+    const Div16 = () => <div className='name'>
         <h1 className='n' id='me'>Tênis</h1>
         <h2 className='time' id='tim'>Camisas de Time</h2>
         <h3 className='cami' id='setas'>Camisetas</h3>
         <h4 className='blu' id='sa'>Blusas</h4>
+    </div>
+
+    const Div17 = () => <div className='linhassss'>
+    </div>
+
+    const Div18 = () => <div className='poster'>
+        <img id='teste20' src="/assets/images/Air_Jordan_poster.png" alt="a" />
+        
+    </div>
+
+    const Div19 = () => <div className='brasil'>
+        <img id='bola1' src="/assets/images/logo.png" alt="" />
+    </div>
+
+    const Div20 = () => <div className='usuario'>
+         <a href='#'><img className="dell" src="/assets/images/User.png" alt="" /></a>
     </div>
     return(
         <>
@@ -158,12 +129,15 @@ export default function inicial(){
         <Div7 />
         <Div8 />
         <Div9 />
-        <Div10 />
-        <Div11 />
-        <Div12 />
-        <Div13 />
-        <Div14 />
-        <Div15 />
+        <Div10/>
+        <Div11/>
+        <Div12/>
+        <Div13/>
+        <Div14/>
+        <Div15/>
+        <Div16/>
+        <Div17/>
+        <Div18/>
         </>
     )
 }
